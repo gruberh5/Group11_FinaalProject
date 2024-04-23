c89137399d24e3a530141307bc6a9ee7a74d34fa
Kyle Hsu <hsukt@mail.uc.edu>
2024-04-22 22:11:52 -0400

    Greyson's Code

--- Group11_FinaalProject/Greyson_Problem.aspx.cs
+++ Group11_FinaalProject/Greyson_Problem.aspx.cs
@@ -13,5 +13,42 @@ namespace Group11_FinaalProject
         {
 
         }
+
+        public class Solution
+        {
+            public string CountAndSay(int n)
+            {
+                string num = "1";
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    Console.WriteLine(num);
+                    num = word(num);
+                }
+                return num;
+            }
+
+            public string word(string num)
+            {
+                string ans = "";
+                char current = num[0];
+                int charCount = 0;
+                for (int i = 0; i < num.Length; i++)
+                {
+                    if (num[i] == current)
+                    {
+                        charCount++;
+                    }
+                    else
+                    {
+                        ans += charCount.ToString() + current;
+                        current = num[i];
+                        charCount = 1;
+                    }
+                }
+                ans += charCount.ToString() + current;
+                return ans;
+            }
+        }
     }
 }
